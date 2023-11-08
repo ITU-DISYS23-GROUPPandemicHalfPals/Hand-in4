3e91e5d0cee07d0ba61a25e5d9c71a28c8fd78dd
Patrick Handberg Jessen <patj@itu.dk>
2023-11-08 11:23:44 +0100

    Create node struct

--- node/node.go
+++ node/node.go
@@ -1,5 +1,37 @@
 package main
 
+import "mutualExclusion/mutualExclusion"
+
+type node struct {
+	Id    int
+	Port  int
+	Ports []int
+
+	Elections    chan *mutualExclusion.ElectionMessage
+	Coordinators chan *mutualExclusion.CoordinatorMessage
+}
+
+func Node(id int, port int) *node {
+	return &node{
+		Id:   id,
+		Port: port,
+
+		Elections:    make(chan *mutualExclusion.ElectionMessage, 100),
+		Coordinators: make(chan *mutualExclusion.CoordinatorMessage, 100),
+	}
+}
+
 func main() {
+	n := Node(1, 5523)
+
+	n.client()
+	n.server()
+}
+
+func (n *node) client() {
+
+}
+
+func (n *node) server() {
 
 }
