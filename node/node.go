1235666b71fa75989afd6cdc8a0738ee256c5341
Patrick Handberg Jessen <patj@itu.dk>
2023-11-08 11:50:21 +0100

    Added flags

--- node/node.go
+++ node/node.go
@@ -1,8 +1,16 @@
 package main
 
-import "mutualExclusion/mutualExclusion"
+import (
+	"flag"
+	"mutualExclusion/mutualExclusion"
+)
+
+var name = flag.String("name", "John Doe", "The name of the node")
+var id = flag.Int("id", 1, "The id of the node")
+var port = flag.Int("port", 5000, "The port of the node")
 
 type node struct {
+	Name  string
 	Id    int
 	Port  int
 	Ports []int
@@ -11,10 +19,22 @@ type node struct {
 	Coordinators chan *mutualExclusion.CoordinatorMessage
 }
 
-func Node(id int, port int) *node {
+func Node(name string, id int, port int) *node {
+	var ports []int
+
+	for i := 5000; i <= 5002; i++ {
+		if i == port {
+			continue
+		}
+
+		ports = append(ports, i)
+	}
+
 	return &node{
-		Id:   id,
-		Port: port,
+		Name:  name,
+		Id:    id,
+		Port:  port,
+		Ports: ports,
 
 		Elections:    make(chan *mutualExclusion.ElectionMessage, 100),
 		Coordinators: make(chan *mutualExclusion.CoordinatorMessage, 100),
@@ -22,16 +42,22 @@ func Node(id int, port int) *node {
 }
 
 func main() {
-	n := Node(1, 5523)
+	flag.Parse()
 
+	n := Node(*name, *id, *port)
+
+	println(n.Name)
+	println(n.Id)
+	println(n.Port)
+
+	go n.server()
 	n.client()
-	n.server()
-}
-
-func (n *node) client() {
-
 }
 
 func (n *node) server() {
+
+}
+
+func (n *node) client() {
 
 }
